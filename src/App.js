fd8123c114221559b314eef7dcc823125b68b748
Henocks <hr4574@naver.com>
2018-08-01 19:34:19 +0900

    API Done. need to fix dismiss func

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+
+const list = [];
 
 /*
 class Developer {
@@ -87,11 +77,19 @@ class App extends Component {
     this.state = {
       list,
       searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     }
 
-    this.onDismiss = this.onDismiss.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
 
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result});
   }
 
   onSearchChange(event) {
@@ -104,7 +102,9 @@ class App extends Component {
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
 
-    this.setState({ list: updatedList });
+    this.setState({
+      list: updatedList
+    });
 
   }
 /* using arrow function. auto binding!
@@ -116,8 +116,11 @@ class App extends Component {
   }
 */
 
-render() {
-    const { searchTerm, list } = this.state;
+  render() {
+    const { searchTerm, result } = this.state;
+
+    if(!result) { return null; }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -129,13 +132,23 @@ render() {
           </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
       </div>
     );
   }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error)
+  }
+
 }
 
 class Button extends Component {
