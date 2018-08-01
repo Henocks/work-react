80e362804ec9a0622895211b7652f48266fefd42
Henocks <hr4574@naver.com>
2018-08-01 15:14:32 +0900

    var 사용

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var helloWorld = '리액트에 오신 여러분을 환영합니다';
     return (
       <div className="App">
-        <h2>리액트에 오신 여러분을 환영합니다</h2>
+        <h2>{helloWorld}</h2>
       </div>
     );
   }
