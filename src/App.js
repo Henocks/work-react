16b0e7a3b9ac0d00f7fb162f8fd856fbaa189382
Henocks <hr4574@naver.com>
2018-08-01 15:55:43 +0900

    React basic : map, DOM, function, class...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 const list = [
@@ -21,9 +21,22 @@ const list = [
   }
 ];
 
+class Developer {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  getName() {
+    return this.firstname + ' ' + this.lastname;
+  }
+}
+
 class App extends Component {
   render() {
-    const helloWorld = '리액트에 오신 여러분을 환영합니다';
+    const robin = new Developer('Robin', 'Wieruch');
+    console.log(robin.getName());
+
     return (
       <div className="App">
         {list.map(item =>
